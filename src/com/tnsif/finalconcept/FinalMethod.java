4fbb9b9a132fa783627908600eeb6d469edd513e
sys <sys@sts-PC>
2024-12-14 11:06:10 +0530

    first commit

---
+++ src/com/tnsif/finalconcept/FinalMethod.java
@@ -0,0 +1,14 @@
+package com.tnsif.finalconcept;
+
+/**
+ *
+ * @author sys
+ */
+public class FinalMethod {
+
+    final int a = 50;
+
+    final void show() {
+        System.out.println("Value of a is" + a);
+    }
+}
