ef9887ac33334f62fb8015b227c100786f5fd7c5
programmer3178 <188353181+programmer3178@users.noreply.github.com>
2024-12-30 22:20:41 +0530

    first commit

---
+++ BasicConcepts/FirstProgram.java
@@ -0,0 +1,15 @@
+
+ 
+package com;
+
+/**
+ * @author sys
+ */
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ BasicConcepts/SecondProgram.java
@@ -0,0 +1,14 @@
+
+package com;
+
+/**
+ * @author sys
+ */
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+
+	}
+
+}
